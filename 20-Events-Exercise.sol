8b48bb39f8642f86c0f4b67eab80c2a1ad8548b5
TresorRw <alaintresorcyusa683@gmail.com>
2024-01-19 17:38:57 +0200

    Finished exercise on events emitting

--- 20-Events-Exercise.sol
+++ 20-Events-Exercise.sol
@@ -3,25 +3,27 @@
 pragma solidity ^0.8.22;
 
 contract EventExample {
-    // 1️⃣ Add an event called "NewUserRegistered" with 2 arguments
-    // 👉 user as address type
-    // 👉 username as string type
+    // 1️⃣ Add an event called "NewUserRegistered" with 2 arguments (done)
+    // 👉 user as address type (done)
+    // 👉 username as string type (done)
     // CODE HERE 👇
-    
+
+    event NewUserRegistered(address indexed user, string username);
+
     struct User {
         string username;
         uint256 age;
     }
-    
+
     mapping(address => User) public users;
-    
+
     function registerUser(string memory _username, uint256 _age) public {
         User storage newUser = users[msg.sender];
         newUser.username = _username;
         newUser.age = _age;
-        
+
         // 2️⃣ Emit the event with msg.sender and username as the inputs
         // CODE HERE 👇
-
+        emit NewUserRegistered(msg.sender, _username);
     }
 }
