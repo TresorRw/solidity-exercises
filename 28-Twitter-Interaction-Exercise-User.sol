93f5a7053322d062d5ed839d76bee3a892505be3
TresorRw <alaintresorcyusa683@gmail.com>
2024-01-23 01:20:38 +0200

    Profile contract to add and get profiles

--- 28-Twitter-Interaction-Exercise-User.sol
+++ 28-Twitter-Interaction-Exercise-User.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-// 1️⃣ Save UserProfile to the mapping in the setProfile() function
+// 1️⃣ Save UserProfile to the mapping in the setProfile() function (done)
 // HINT: don't forget to set the _displayName and _bio
 
 pragma solidity ^0.8.22;
@@ -10,15 +10,22 @@ contract Profile {
         string displayName;
         string bio;
     }
-    
+
     mapping(address => UserProfile) public profiles;
 
     function setProfile(string memory _displayName, string memory _bio) public {
         // CODE HERE 👇
+        UserProfile memory newUserProfile = UserProfile({
+            displayName: _displayName,
+            bio: _bio
+        });
 
+        profiles[msg.sender] = newUserProfile;
     }
 
-    function getProfile(address _user) public view returns (UserProfile memory) {
+    function getProfile(
+        address _user
+    ) public view returns (UserProfile memory) {
         return profiles[_user];
     }
 }
